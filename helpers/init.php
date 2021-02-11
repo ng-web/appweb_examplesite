9b7bc6fccce3463dc2cfe746d47befc5d5c6aa8f
ng-web <likeslocalja@gmail.com>
2021-02-11 15:37:04 -0500

    User Registration System and Sessions

---
+++ helpers/init.php
@@ -0,0 +1,7 @@
+<?php
+include 'conn.php';
+include 'helper.php';
+
+//and any other php files to include
+//https://codeburst.io/how-to-maintain-core-php-projects-594721858cad
+//https://guides.codepath.com/websecurity/PHP-Code-organization#code-organization
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>My Website</title>
+
+
+</head>
+
+<body>
+
+<?php include('views/sub-views/header.php'); ?>
+
+<section id="main">
+
+	<?php include('views/sub-views/sidebar.php'); ?>
+
+	<section id="content">
+		<!-- content begins -->
+
+
+		<!-- content ends -->
+	</section>
+  
+  <?php include('views/sub-views/footer.php'); ?> 
+
+</section>
+
+</body>
+</html>
